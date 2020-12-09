66763ac51ba023e0e2319efd14669c74e9002d12
AnahitKhecumyan <Khechumyan63@gmail.com>
2020-12-10 00:18:04 +0300

    using  actions and react-toastify

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,73 @@
+import * as actionTypes from './actionTypes';
+
 const defaultState = {
-    tasks: [] ,
+    tasks: [],
+    loading: false,
+    error: null,
+    addTaskSuccess: false,
+    successMessage: ''
   };
 
   export const mainReducer = (state = defaultState, action)=>{
     switch(action.type){
-      case 'GET_TASKS_SUCCESS':{
+       case actionTypes.LOADING:{
+         return{
+            ...state,
+            loading: true
+         };
+       }
+
+       case actionTypes.ERROR:{
         return{
-          ...state,
-          tasks: action.tasks
-        }
+           ...state,
+           loading: false,
+           error: action.error
+        };
       }
+      
+       case actionTypes.GET_TASKS_SUCCESS:{
+        return{
+           ...state,
+           loading: false,
+           tasks: action.tasks
+           
+        };
+      }
+
+      case actionTypes.ADDING_TASK:{
+        return{
+           ...state,
+           loading: true,
+           addTaskSuccess: false,
+           successMessage: null,
+           error: null
+        };
+      }
+
+
+      case actionTypes.ADD_TASK_SUCCESS:{
+        return{
+           ...state,
+           loading: false,
+           tasks: [...state.tasks, action.task],
+           addTaskSuccess: true,
+           successMessage:'Task added successfully'
+        };
+      }
+       
+      case actionTypes.EDIT_TASK_SUCCESS:{
+        const tasks = [state.tasks];
+        const foundIndex = tasks.findIndex(task => task._id === action.editedTask._id );
+        tasks[foundIndex] = action.editedTask;
+
+
+       return{
+          ...state,
+          loading: false,
+          tasks: tasks
+       };
+     }
+     
      default: return state;
    }
   };
